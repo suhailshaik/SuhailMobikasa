8ca849bf073698fae8630ccc1345d023192be3d9
suhailshaik <shaiksuhail.ui@gmail.com>
2015-10-25 19:45:19 -0400

    Navigation bar change on scroll.

--- js/script.js
+++ js/script.js
@@ -1,58 +1,57 @@
-
 //SMOOTH PAGE SCROLL
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
-
 
 //OWL CAROSEL TESTIMONIAL
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots:true,
-    dotsEach:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    dotsEach: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:1
+        600: {
+            items: 1
         },
-        1000:{
-            items:1
+        1000: {
+            items: 1
         }
     }
 })
 
 $(document).ready(
-
-  function() { 
-
-    $("html").niceScroll({
-    	cursorcolor:"#F69220",
-    	scrollspeed :"100",
-    	cursorborder:"1px solid #F69220",
-    	horizrailenabled: "false",
-    	cursorborderradius: "0px"
-    });
-
-  }
-
+    function() {
+        $("html").niceScroll({
+            cursorcolor: "#F69220",
+            scrollspeed: "100",
+            cursorborder: "1px solid #F69220",
+            horizrailenabled: "false",
+            cursorborderradius: "0px"
+        });
+    }
 );
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 51) {
+       console.log("Scroll > 50px")
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
 
-// new WOW().init();
-
-
-
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
